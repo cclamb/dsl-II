ba58aa3295c694387b33cf345f0e5aff5fb0ff5a
cclamb <cclamb@cs.unm.edu>
2011-04-11 05:58:30 -0600

    refactored executor out

--- lib/insomnia/activity.rb
+++ lib/insomnia/activity.rb
@@ -1,19 +1,7 @@
-
+require_relative 'deferred_executor'
 
 def activity(name, &b)
   Activity.new(&b)
 end
 
-class Activity
-
-  attr_accessor :block
-
-  def initialize(&b)
-    @block = b
-  end
-
-  def call
-    @block != nil ? block.call : true
-  end
-
-end
+class Activity < DeferredExecutor; end
---
+++ lib/insomnia/deferred_executor.rb
@@ -0,0 +1,13 @@
+class DeferredExecutor
+
+  attr_accessor :block
+
+  def initialize(&b)
+    @block = b
+  end
+
+  def call
+    @block != nil ? block.call : true
+  end
+
+end
