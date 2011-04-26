ee32a587b51cfa83f38a6217b38dbf930fd05dee
cclamb <cclamb@cs.unm.edu>
2011-04-26 05:52:17 -0600

    work

--- lib/insomnia/policy.rb
+++ lib/insomnia/policy.rb
@@ -1,3 +1,18 @@
 class Policy
 
+  attr_accessor :restricted_activities, :obligators
+
+  def initialize(&b)
+    instance_exec(&b) if block_given?
+  end
+  
+  def obligate(*args)
+    @restricted_activities = []
+    @obligators = []
+    args.each do |x|
+      @restricted_activities.push(x) if x.kind_of? Restrict
+      @obligators = @obligators.push(x[:with]).flatten if x.kind_of? Hash
+    end
+  end
+
 end
--- test/rspec/policy_rspec.rb
+++ test/rspec/policy_rspec.rb
@@ -1,14 +1,44 @@
+require_relative '../../lib/insomnia/restrict'
 require_relative '../../lib/insomnia/policy'
 
 describe Policy do
 
   context 'with no obligations' do
-    it 'should handle empty policies'
+    it 'should handle empty policies' do
+      p = Policy.new
+      p.should_not == nil
+    end
   end
 
   context 'with obligations' do
-    it 'should handle single obligations'
-    it 'should handle multiple obligations'
+    it 'should handle single obligations' do
+      ra1 = Restrict.new
+      ra2 = Restrict.new
+      p = Policy.new do
+        obligate ra1, :with => ra2
+      end
+      p.restricted_activities.size.should == 1
+      p.obligators.size.should == 1
+      p.restricted_activities[0].should == ra1
+      p.obligators[0].should == ra2
+    end
+    
+    it 'should handle multiple obligations' do
+      fail 'policy implementation cannot support this; need obligation class'
+      ra1 = Restrict.new
+      ra2 = Restrict.new
+      ra3 = Restrict.new
+      ra4 = Restrict.new
+      p = Policy.new do
+        obligate ra1, :with => ra2
+        obligate ra3, :with => ra4
+      end
+      p.restricted_activities.size.should == 1
+      p.obligators.size.should == 1
+      p.restricted_activities[0].should == ra1
+      p.obligators[0].should == ra2
+    end
+    
     it 'should handle one-to-one obligations'
     it 'should handle one-to-many obligations'
   end
