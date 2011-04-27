02b1f43e9e0268d4304bf7acf17ed9e8fac39210
Chris Lamb <cclamb@cs.unm.edu>
2011-04-27 16:28:50 -0600

    additional policy tests

--- lib/insomnia/policy.rb
+++ lib/insomnia/policy.rb
@@ -2,19 +2,21 @@ require_relative 'obligation'
 
 class Policy
 
-  attr_accessor :restricted_activities, :obligators
+  attr_accessor :obligations
 
   def initialize(&b)
+    @obligations = []
     instance_exec(&b) if block_given?
   end
   
   def obligate(*args)
-    @restricted_activities = []
-    @obligators = []
+    restricted_activities = []
+    obligators = []
     args.each do |x|
-      @restricted_activities.push(x) if x.kind_of? Restrict
-      @obligators = @obligators.push(x[:with]).flatten if x.kind_of? Hash
+      restricted_activities.push(x) if x.kind_of? Restrict
+      obligators = obligators.push(x[:with]).flatten if x.kind_of? Hash
     end
+    obligations.push(Obligation.new(restricted_activities, obligators))
   end
 
 end
--- test/rspec/policy_rspec.rb
+++ test/rspec/policy_rspec.rb
@@ -18,14 +18,12 @@ describe Policy do
       p = Policy.new do
         obligate ra1, :with => ra2
       end
-      p.restricted_activities.size.should == 1
-      p.obligators.size.should == 1
-      p.restricted_activities[0].should == ra1
-      p.obligators[0].should == ra2
+      p.obligations.size.should == 1
+      p.obligations[0].obligatees[0].should == ra1
+      p.obligations[0].obligators[0].should == ra2
     end
     
     it 'should handle multiple obligations' do
-      fail 'policy implementation cannot support this; need obligation class'
       ra1 = Restrict.new
       ra2 = Restrict.new
       ra3 = Restrict.new
@@ -34,13 +32,24 @@ describe Policy do
         obligate ra1, :with => ra2
         obligate ra3, :with => ra4
       end
-      p.restricted_activities.size.should == 1
-      p.obligators.size.should == 1
-      p.restricted_activities[0].should == ra1
-      p.obligators[0].should == ra2
+      p.obligations.size.should == 2
+      p.obligations[0].obligatees[0].should == ra1
+      p.obligations[0].obligators[0].should == ra2
+      p.obligations[1].obligatees[0].should == ra3
+      p.obligations[1].obligators[0].should == ra4
     end
     
-    it 'should handle one-to-one obligations'
+    it 'should handle one-to-one obligations' do
+      ra1, ra2, ra3 = Restrict.new, Restrict.new, Restrict.new
+      p = Policy.new do
+        obligate ra1, :with => [ra2, ra3]
+      end
+      p.obligations.size.should == 1
+      p.obligations[0].obligatees[0].should == ra1
+      p.obligations[0].obligators[0].should == ra2
+      p.obligations[0].obligators[1].should == ra3
+    end
+    
     it 'should handle one-to-many obligations'
   end
 
